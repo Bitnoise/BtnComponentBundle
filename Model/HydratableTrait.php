9d97f70df12e1ef5b0e3268c22f37571bb8d30d7
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-06 13:54:17 +0200

    add hydratable trair
    for future

---
+++ Model/HydratableTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Users\Projects\Btn\WebplatformBundle\Model;
+
+/**
+ * For better times :P
+ */
+trait HydratableTrait
+{
+    /**
+     *
+     */
+    protected $hydrated = false;
+
+    /**
+     *
+     */
+    public function isHydrated()
+    {
+        return $this->hydrated;
+    }
+
+    /**
+     *
+     */
+    public function hydrated()
+    {
+        $this->hydrated = true;
+
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function isDried()
+    {
+        return !$this->hydrated;
+    }
+
+    /**
+     *
+     */
+    public function dried()
+    {
+        $this->hydrated = false;
+
+        return $this;
+    }
+}
