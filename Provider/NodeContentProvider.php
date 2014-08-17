6e0a48447f0938c0542bdbcd47bcfaeec3f684e1
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-17 15:20:03 +0200

    add doc blocks

--- Provider/NodeContentProvider.php
+++ Provider/NodeContentProvider.php
@@ -13,12 +13,18 @@ class NodeContentProvider implements NodeContentProviderInterface
     private $containersProvider;
     private $renderRouteName;
 
+    /**
+     *
+     */
     public function __construct($containersProvider, $renderRouteName)
     {
         $this->containersProvider = $containersProvider;
         $this->renderRouteName    = $renderRouteName;
     }
 
+    /**
+     *
+     */
     public function getForm()
     {
         $data = array();
@@ -29,26 +35,41 @@ class NodeContentProvider implements NodeContentProviderInterface
         return new NodeContentType($data);
     }
 
+    /**
+     *
+     */
     public function resolveRoute($formData = array())
     {
         return $this->renderRouteName;
     }
 
+    /**
+     *
+     */
     public function resolveRouteParameters($formData = array())
     {
         return array('id' => $formData['container']);
     }
 
+    /**
+     *
+     */
     public function resolveControlRoute($formData = array())
     {
         return 'btn_component_containercontrol_edit';
     }
 
+    /**
+     *
+     */
     public function resolveControlRouteParameters($formData = array())
     {
         return array('id' => $formData['container']);
     }
 
+    /**
+     *
+     */
     public function getName()
     {
         return 'Page components';
