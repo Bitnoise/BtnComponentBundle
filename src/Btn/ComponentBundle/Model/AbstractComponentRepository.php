4509720ab72378ea86687a4fb5aff5cebe8a55ee
Adam Misiorny <adam.misiorny@gmail.com>
2014-10-06 11:03:14 +0200

    remove  getMaxPositionForContainer

--- src/Btn/ComponentBundle/Model/AbstractComponentRepository.php
+++ src/Btn/ComponentBundle/Model/AbstractComponentRepository.php
@@ -6,17 +6,4 @@ use Gedmo\Sortable\Entity\Repository\SortableRepository;
 
 abstract class AbstractComponentRepository extends SortableRepository
 {
-    public function getMaxPositionForContainer($container)
-    {
-        $qb = $this->createQueryBuilder('component');
-
-        $qb
-            ->select('MAX(component.position)')
-            ->where('component.container = :container')
-            ->setParameter('container', $container);
-
-        $q = $qb->getQuery();
-
-        return (int) $q->getSingleScalarResult();
-    }
 }
